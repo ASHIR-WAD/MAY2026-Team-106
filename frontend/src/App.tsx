03bb03c1cdb0457eb0132fef387aebddb7f1087a
Prashant Kumar <prashantkumarbr@gmail.com>
2026-07-11 15:05:15 +0530

    initial setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <>
+      <section>
+        <div>
+          <h1 className="text-4xl font-bold text-blue-600">
+            Event Management App
+          </h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
