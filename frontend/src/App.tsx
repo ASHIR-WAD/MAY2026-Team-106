db99e3ecd5b4509160a2483001bc13b923878b03
Prashant Kumar <prashantkumarbr@gmail.com>
2026-07-15 13:47:45 +0530

    API Client & Mock Data Toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
+import { useEffect } from 'react'
+import { apiGet } from './lib/api'
+
 function App() {
-  return <div className="min-h-screen bg-bg" />
+  useEffect(() => {
+    async function test() {
+      try {
+        const events = await apiGet('/events')
+        console.log(events)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    test()
+  }, [])
+
+  return <h1>Testing API</h1>
 }
 
 export default App
